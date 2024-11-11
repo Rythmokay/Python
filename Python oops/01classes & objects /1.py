5e83517e1d8538a6394a7c7313b9fa6417dab2af
Rythmokay <129789738+Rythmokay@users.noreply.github.com>
2024-11-11 01:56:24 +0000

    committ

---
+++ Python oops/01classes & objects /1.py
@@ -0,0 +1,21 @@
+#creating class 
+class Student :
+    name ="Rythm Jagga"
+
+#creating object 
+#objects are basically instance of class  
+# creating first object 
+s1=Student()
+print(s1.name)
+
+# creating second object
+s2=Student()
+print(s2.name)
+
+# class Car :
+#     color ="blue"
+#     brand="mercedes"
+
+# car1=Car()
+# print(car1.color)
+# print(car1.brand)
